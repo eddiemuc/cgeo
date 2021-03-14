54620235b0557ee99352658bda1795845c760c6e
eddiemuc <andreas-kopp@gmx.net>
2021-03-14 10:29:17 +0100

    BRouter: attempt to rewrite PhysicalFile to work with SAF

---
+++ main/src/cgeo/geocaching/brouter/mapaccess/PhysicalFile2.java
@@ -0,0 +1,163 @@
+/**
+ * cache for a single square
+ *
+ * @author ab
+ */
+package cgeo.geocaching.brouter.mapaccess;
+
+import org.mapsforge.map.reader.IndexCache;
+import org.mapsforge.map.reader.ReadBuffer;
+import org.mapsforge.map.reader.header.MapFileHeader;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.logging.Level;
+
+import cgeo.geocaching.brouter.codec.DataBuffers;
+import cgeo.geocaching.brouter.util.ByteDataReader;
+import cgeo.geocaching.brouter.util.Crc32;
+import cgeo.geocaching.utils.Log;
+import cgeo.geocaching.utils.UriUtils;
+
+final public class PhysicalFile2 {
+    public long creationTime;
+    public int divisor = 80;
+    //RandomAccessFile ra = null; //use "close()" method instead!
+    long[] fileIndex = new long[25];
+    int[] fileHeaderCrcs;
+    //String fileName;
+    private final int fileIndexCrc;
+
+    private FileChannel inputChannel;
+
+    public PhysicalFile2(FileInputStream fis, DataBuffers dataBuffers, int lookupVersion, int lookupMinorVersion) throws Exception {
+        //fileName = UriUtils.getLastPathSegment(uri);
+
+        inputChannel = fis.getChannel();
+        ReadBuffer readBuffer = new ReadBuffer(this.inputChannel);
+//        this.mapFileHeader = new MapFileHeader();
+//        this.mapFileHeader.readHeader(readBuffer, this.fileSize);
+//        this.databaseIndexCache = new IndexCache(this.inputChannel, INDEX_CACHE_SIZE);
+//
+//        this.timestamp = lastModified;
+
+
+        byte[] iobuffer = dataBuffers.iobuffer;
+        ByteBuffer bb = ByteBuffer.wrap(iobuffer);
+        inputChannel.read(bb, 0, 200);
+        ra = new RandomAccessFile(f, "r");
+        ra.readFully(iobuffer, 0, 200);
+        fileIndexCrc = Crc32.crc(iobuffer, 0, 200);
+        ByteDataReader dis = new ByteDataReader(iobuffer);
+        for (int i = 0; i < 25; i++) {
+            long lv = dis.readLong();
+            short readVersion = (short) (lv >> 48);
+            if (i == 0 && lookupVersion != -1 && readVersion != lookupVersion) {
+                throw new IllegalArgumentException("lookup version mismatch (old rd5?) lookups.dat="
+                    + lookupVersion + " " + f.getAbsolutePath() + "=" + readVersion);
+            }
+            fileIndex[i] = lv & 0xffffffffffffL;
+        }
+
+        // read some extra info from the end of the file, if present
+        long len = ra.length();
+
+        long pos = fileIndex[24];
+        int extraLen = 8 + 26 * 4;
+
+        if (len == pos) return; // old format o.k.
+
+        if (len < pos + extraLen) // > is o.k. for future extensions!
+        {
+            throw new IOException("file of size " + len + " too short, should be " + (pos + extraLen));
+        }
+
+        ra.seek(pos);
+        ra.readFully(iobuffer, 0, extraLen);
+        dis = new ByteDataReader(iobuffer);
+        creationTime = dis.readLong();
+
+        int crcData = dis.readInt();
+        if (crcData == fileIndexCrc) {
+            divisor = 80; // old format
+        } else if ((crcData ^ 2) == fileIndexCrc) {
+            divisor = 32; // new format
+        } else {
+            throw new IOException("top index checksum error");
+        }
+        fileHeaderCrcs = new int[25];
+        for (int i = 0; i < 25; i++) {
+            fileHeaderCrcs[i] = dis.readInt();
+        }
+    }
+
+    public void close() {
+        closeFileChannel();
+    }
+
+    /**
+     * Closes the map file channel and destroys all internal caches.
+     * Has no effect if no map file channel is currently opened.
+     */
+    private void closeFileChannel() {
+        try {
+            if (this.inputChannel != null) {
+                this.inputChannel.close();
+            }
+        } catch (Exception e) {
+            Log.e(e.getMessage(), e);
+        }
+    }
+
+//    public static void main(String[] args) {
+//        MicroCache.debug = true;
+//
+//        String message = checkFileIntegrity(new File(args[0]));
+//
+//        if (message != null) {
+//            System.out.println("************************************");
+//            System.out.println(message);
+//            System.out.println("************************************");
+//        }
+//    }
+//
+//    /**
+//     * Checks the integrity of the file using the build-in checksums
+//     *
+//     * @return the error message if file corrupt, else null
+//     */
+//    public static String checkFileIntegrity(File f) {
+//        PhysicalFile pf = null;
+//        try {
+//            DataBuffers dataBuffers = new DataBuffers();
+//            pf = new PhysicalFile(f, dataBuffers, -1, -1);
+//            int div = pf.divisor;
+//            for (int lonDegree = 0; lonDegree < 5; lonDegree++) // does'nt really matter..
+//            {
+//                for (int latDegree = 0; latDegree < 5; latDegree++) // ..where on earth we are
+//                {
+//                    OsmFile osmf = new OsmFile(pf, lonDegree, latDegree, dataBuffers);
+//                    if (osmf.hasData())
+//                        for (int lonIdx = 0; lonIdx < div; lonIdx++)
+//                            for (int latIdx = 0; latIdx < div; latIdx++)
+//                                osmf.createMicroCache(lonDegree * div + lonIdx, latDegree * div + latIdx, dataBuffers, null, null, MicroCache.debug, null);
+//                }
+//            }
+//        } catch (IllegalArgumentException iae) {
+//            return iae.getMessage();
+//        } catch (Exception e) {
+//            return e.toString();
+//        } finally {
+//            if (pf != null)
+//                try {
+//                    pf.ra.close();
+//                } catch (Exception ignored) {
+//                }
+//        }
+//        return null;
+//    }
+}
